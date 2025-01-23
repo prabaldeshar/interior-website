8c9c94aca5500c52bc123d14b050582eaa010ae7
Prabal Deshar <prabaldeshar@gmail.com>
2025-01-23 09:29:41 +0545

    Refactor project data fetching in ProjectList; update Card component structure for improved layout

--- components/ProjectList.js
+++ components/ProjectList.js
@@ -1,8 +1,12 @@
 import Card from "./Card";
-import getProjects from "../helpers/loadJson";
+// import getProjects from "../helpers/loadJson";
 
 const ProjectList = async () => {
-    const projects = await getProjects();
+    const response = await fetch('http://localhost:8001/project/list/')
+    const data = await response.json()
+    const projects = data.projects
+    
+    // const projects = await getProjects();
     return (
         <section className="py-5 bg-light" id="projects">
         <div className="container">
@@ -10,9 +14,7 @@ const ProjectList = async () => {
             <p className="text-center mb-5" style={{ fontSize: "1.3rem" }}>
             Explore some of the stunning projects we've completed for our clients.
             </p>
-            <div className="row g-4">
-            
-            
+            <div className="row row-cols-1 row-cols-md-3 g-4">
             {projects.map((project) => (
                 <Card key={project.id} project={project}/>
             ))}
