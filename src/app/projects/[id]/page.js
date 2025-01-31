102947d738168d722fec24fbf4cbfec3aad330c7
Prabal Deshar <prabaldeshar@gmail.com>
2025-01-31 13:37:25 +0545

    update: switch BASE_URL to HTTPS and disable TLS certificate validation for development

--- src/app/projects/[id]/page.js
+++ src/app/projects/[id]/page.js
@@ -7,6 +7,8 @@ import NavBar from "../../../../components/NavBar";
 import Footer from "../../../../components/Footer";
 import { BASE_URL } from '../../../../constants/constants';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const Details = async ({ params }) => {
   const params_data = await params;
 
