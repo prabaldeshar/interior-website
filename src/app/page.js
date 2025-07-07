ad9a6bfc8eb945c3cca039bee877ddbcd36d222a
Prabal Deshar <prabaldeshar@gmail.com>
2025-07-07 18:13:51 +0545

    fix: update ReCAPTCHA site key and enhance services image styles

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,7 @@ import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 import HeroSection from "../../components/HeroSection";
 import ProjectList from "../../components/ProjectList";
-import 'bootstrap/dist/css/bootstrap.css'
-import "./globals.css";
+// moved Bootstrap and global styles into root layout
 import Services from "../../components/Services";
 import HomeHeroSection from "../../components/HomeHeroSection";
 import AboutUs from "../../components/AboutUs";
@@ -13,7 +12,6 @@ import ServicesSection from "../../components/ServicesHomeSection";
 const Home = () => {
   return (
     <div className="mb-3 mr-2 ml-2 bg-white">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
 
     <NavBar />
     <HomeHeroSection />
